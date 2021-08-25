2756343f05ca70420ed964290bf1afa3711c025f
IvanPy96 <64599936+IvanPy96@users.noreply.github.com>
2021-08-25 10:24:57 +0300

    Add files via upload

--- inference.py
+++ inference.py
@@ -3,7 +3,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision import datasets, models, transforms
+from torchvision import models, transforms
 
 import os
 from sys import argv
@@ -12,23 +12,13 @@ import requests
 
 from PIL import Image
 import matplotlib.pyplot as plt 
-%matplotlib inline
-%config InlineBackend.figure_format = 'retina'
 
-file_link = argv
+script, file_link = argv      
 
-# Commented out IPython magic to ensure Python compatibility.
-%mkdir test_data
-
-urls = [
-        r'file_link'
-        ]
-
-for ii, url in enumerate(urls):
-    response = requests.get(url, stream=True)
-    with open('test_data/img_test_' + str(ii) + '.jpg', 'wb') as out_file:
-        shutil.copyfileobj(response.raw, out_file)
-    del response
+response = requests.get(file_link, stream=True)
+with open('img_test.jpg', 'wb') as out_file:
+    shutil.copyfileobj(response.raw, out_file)
+del response
     
 
 test_transforms = transforms.Compose([transforms.CenterCrop(320),
@@ -41,7 +31,7 @@ device = torch.device('cpu')
 model = models.resnet50(pretrained=False)
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(num_ftrs, 10)
-model.load_state_dict(torch.load('https://drive.google.com/file/d/1umw8zS6LUk3ntVbHHRzRvXBrpZcdxYrZ/view?usp=sharing', map_location=device))
+model.load_state_dict(torch.load('/Users/mordovets.i/imagewoof_ResNet50.pth', map_location=device))
 model.eval()
 
 class_names = ['Shih-Tzu', 'Rhodesian ridgeback', 'Beagle', 'English foxhound', 
@@ -52,8 +42,7 @@ fig = plt.figure(figsize=(20,10))
 
 with torch.no_grad():
 
-    for ii, file_name in enumerate(os.listdir( './test_data' )):
-        img = Image.open( './test_data' + '/' + file_name)
+        img = Image.open('img_test.jpg')
         img_t = test_transforms(img).unsqueeze(0)
         img_t = img_t.to(device)
 
@@ -62,7 +51,6 @@ with torch.no_grad():
         prob = F.softmax(outputs, dim=1)
         top_p, top_class = prob.topk(1, dim = 1)
 
-        ax = plt.subplot(len(os.listdir( '/content/test_data' )),2, ii+1)
-        ax.axis('off')
-        ax.set_title('Порода: {}\n Уверенность модели: {}%'.format(class_names[int(preds.cpu().numpy())], 100*round(top_p.detach().cpu().numpy().tolist()[0][0],2)))
+        plt.title('Порода: {}\n Уверенность модели: {}%'.format(class_names[int(preds.cpu().numpy())], 100*round(top_p.detach().cpu().numpy().tolist()[0][0],2)))
         plt.imshow(img)
+        plt.show()
